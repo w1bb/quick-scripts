047260fc0b6a1c10fe923f3323101484e25030dd
Valentin-Ioan VINTILĂ <45993965+w1bb@users.noreply.github.com>
2024-01-16 23:02:21 +0200

    Make internet-blocker.bat safer (check for confirmation)

--- windows/internet-blocker.bat
+++ windows/internet-blocker.bat
@@ -1,9 +1,33 @@
-@ setlocal enableextensions 
-@ cd /d "[INSERT PATH HERE]"
-
-for /R %%f in (*.exe) do (
-  netsh advfirewall firewall add rule name="Blocked: %%f" dir=in program="%%f" action=block
-  netsh advfirewall firewall add rule name="Blocked: %%f" dir=out program="%%f" action=block
+@setlocal enableextensions 
+@cd /d "[INSERT PATH HERE]"
+@cls
+@echo Here is a list of the files that will be modified:
+@for /R %%f in (*.exe) do @ (
+	echo - %%f
 )
+@ echo.
 
-pause
+:confirm
+@set /p DUMMY=Type Y to proceed or N to cancel. 
+@if %DUMMY% == Y goto startjob
+@if %DUMMY% == N goto canceljob
+@goto confirm
+
+:startjob
+@echo.
+@for /R %%f in (*.exe) do @ (
+ 	echo Altering %%f
+	netsh advfirewall firewall add rule name="Blocked: %%f" dir=in program="%%f" action=block
+	netsh advfirewall firewall add rule name="Blocked: %%f" dir=out program="%%f" action=block
+)
+@goto end
+
+:canceljob
+@echo.
+@echo No rule was added.
+@goto end
+
+
+:end
+@pause
+@echo.
