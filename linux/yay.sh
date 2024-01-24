f086ae048c7b05875af720920f6a0bca81975377
Valentin-Ioan Vintila <vintilavalentinioan@gmail.com>
2024-01-24 12:54:25 +0200

    Add kitty. Fix tmux and yay.sh.

--- linux/yay.sh
+++ linux/yay.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Update everything
-pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm
 
-# Install yay
-pacman -S --noconfirm git
+# Install git
+sudo pacman -S --noconfirm git
+
+# Clone the repository and make sure it belongs to the current user
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-chown -R wi ./yay-git/
-cd yay-git/
+current_user=$(whoami)
+sudo chown -R $current_user ./yay-git/
+
+# Finally, install yay
+cd ./yay-git/
 makepkg -si
 cd ~
+
